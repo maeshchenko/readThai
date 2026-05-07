00c7b4079d405afd6272c4dd53f734592ed08992
Mikhail Eshchenko <maeschenko@alfabank.ru>
2026-05-07 19:20:36 +0300

    сделал дефолтный язык языком системы

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,7 +3,8 @@ import { initReactI18next } from 'react-i18next'
 import en from './en.json'
 import ru from './ru.json'
 
-const savedLang = localStorage.getItem('lang') || 'en'
+const systemLang = navigator.language.startsWith('ru') ? 'ru' : 'en'
+const savedLang = localStorage.getItem('lang') || systemLang
 
 i18n.use(initReactI18next).init({
   resources: {
